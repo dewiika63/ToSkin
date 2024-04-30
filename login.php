5b9512f0e002672c77ef8915384709d9f108a5cd
dewiika63 <dewiika6303@gmail.com>
2024-04-30 11:06:15 +0700

    Feat : Login

--- login.php
+++ login.php
@@ -28,7 +28,7 @@ if (isset($_POST['submit'])) {
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Login - Padang Jaya</title>
+    <title>Login - Padang Jaya Dewi Ika</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/png" href="assets/images/icon/favicon.ico">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
